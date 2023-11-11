6089554d20fd5ef7c7995889e45c256621aa4ea7
Lucas da Paz <xlucaspx@gmail.com>
2023-11-11 15:15:10 -0300

    modificando .gitignore

--- src/main/java/models/Produto.java
+++ src/main/java/models/Produto.java
@@ -40,7 +40,7 @@ public class Produto {
 
 	private void setCodigo(String codigo) {
 		if (!validaString(codigo)) throw new ValidationException("O código inserido é inválido!");
-		
+
 		this.codigo = codigo;
 	}
 
@@ -50,7 +50,7 @@ public class Produto {
 
 	private void setDescricao(String descricao) {
 		if (!validaString(descricao)) throw new ValidationException("A descrição inserida é inválida!");
-		
+
 		this.descricao = descricao;
 	}
 
@@ -59,9 +59,11 @@ public class Produto {
 	}
 
 	private void setPrecoCusto(float precoCusto) {
-		if (precoCusto <= 0) throw new ValidationException("O preço inserido é inválido; por favor, insira um valor maior do que 0!");
-		if (precoCusto >= precoVenda) throw new ValidationException("O preço de custo deve ser menor do que o preço de venda!");
-		
+		if (precoCusto <= 0)
+			throw new ValidationException("O preço inserido é inválido; por favor, insira um valor maior do que 0!");
+		if (precoCusto >= precoVenda)
+			throw new ValidationException("O preço de custo deve ser menor do que o preço de venda!");
+
 		this.precoCusto = precoCusto;
 	}
 
@@ -71,7 +73,7 @@ public class Produto {
 
 	private void setFormato(Formato formato) {
 		if (formato == null) throw new ValidationException("O formato selecionado é inválido!");
-		
+
 		this.formato = formato;
 	}
 
@@ -80,8 +82,9 @@ public class Produto {
 	}
 
 	private void setQuantidade(double quantidade) {
-		if (quantidade <= 0) throw new ValidationException("A quantidade inserida é inválida; por favor, insira um valor maior do que 0!");
-		
+		if (quantidade <= 0)
+			throw new ValidationException("A quantidade inserida é inválida; por favor, insira um valor maior do que 0!");
+
 		this.quantidade = quantidade;
 	}
 
@@ -90,8 +93,9 @@ public class Produto {
 	}
 
 	private void setImposto(float imposto) {
-		if (imposto < 0) throw new ValidationException("O imposto inserido é inválido; por favor, insira um valor maior ou igual a 0!");
-		
+		if (imposto < 0)
+			throw new ValidationException("O imposto inserido é inválido; por favor, insira um valor maior ou igual a 0!");
+
 		this.imposto = imposto;
 	}
 
@@ -100,9 +104,11 @@ public class Produto {
 	}
 
 	private void setPrecoVenda(float precoVenda) {
-		if (precoVenda <= 0) throw new ValidationException("O preço de venda inserido é inválido; por favor, insira um valor maior do que 0!");
-		if (precoVenda <= precoCusto) throw new ValidationException("O preço de venda não pode ser menor do que o preço de custo!");
-		
+		if (precoVenda <= 0)
+			throw new ValidationException("O preço de venda inserido é inválido; por favor, insira um valor maior do que 0!");
+		if (precoVenda <= precoCusto)
+			throw new ValidationException("O preço de venda não pode ser menor do que o preço de custo!");
+
 		this.precoVenda = precoVenda;
 	}
 
@@ -112,10 +118,8 @@ public class Produto {
 				"Código: %s\nDescrição: %s\nQuantidade: %.2f\nFormato: %s\nPreço custo: R$ %.2f\nPreço venda: R$ %.2f\nImposto: ",
 				codigo, descricao, quantidade, formato, precoCusto, precoVenda);
 
-		if (imposto > 0)
-			produto += String.format("%.0f%%", imposto * 100);
-		else
-			produto += "não cadastrado!";
+		if (imposto > 0) produto += String.format("%.0f%%", imposto * 100);
+		else produto += "não cadastrado!";
 
 		return produto;
 	}
