d2941a7c1861df1eb4eac6b0c933da70ddaa265d
Lucas da Paz <xlucaspx@gmail.com>
2023-11-11 14:57:41 -0300

    FIX: corrigindo SQL de criação e inserção

--- db/create_db.sql
+++ db/create_db.sql
@@ -3,12 +3,12 @@ USE projeto_integrador;
 
 CREATE TABLE fornecedor (
 	cnpj VARCHAR(14) PRIMARY KEY,
-	nome VARCHAR(45) NOT NULL,
+	nome VARCHAR(75) NOT NULL,
 	email VARCHAR(45) NOT NULL,
 	telefone VARCHAR(20) NOT NULL,
-	cep VARCHAR(8) NOT NULL,
+	cep VARCHAR(9) NOT NULL,
 	endereco VARCHAR(45) NOT NULL,
-	numero SMALLINT NOT NULL,
+	numero VARCHAR(10) NOT NULL,
 	complemento VARCHAR(45),
 	bairro VARCHAR(45) NOT NULL,
 	cidade VARCHAR(45) NOT NULL,
@@ -21,15 +21,15 @@ CREATE TABLE produto (
 	preco_custo DECIMAL(7, 2) NOT NULL,
 	preco_venda DECIMAL(7, 2) NOT NULL,
 	formato VARCHAR(3) NOT NULL,
-	estoque FLOAT(7, 3) NOT NULL
+	estoque DECIMAL(7, 3) NOT NULL
 );
 
 CREATE TABLE nota_compra (
 	numero VARCHAR(9) PRIMARY KEY,
-	data_compra VARCHAR(10) NOT NULL,
+	data_compra DATE NOT NULL,
 	fornecedor VARCHAR(14) NOT NULL,
 	CONSTRAINT FK_fornecedor_compra FOREIGN KEY (fornecedor)
-		REFERENCES fornecedor(cnpj)
+		REFERENCES fornecedor (cnpj)
 );
 
 CREATE TABLE item_nota_compra (
@@ -37,13 +37,12 @@ CREATE TABLE item_nota_compra (
 	codigo_produto VARCHAR(50) NOT NULL,
 	quantidade INT NOT NULL,
 	preco DECIMAL(7, 2) NOT NULL,
-	imposto DECIMAL(4, 2) NOT NULL,
-	PRIMARY KEY (numero_nota, codigo_produto),
+	PRIMARY KEY (numero_nota , codigo_produto),
 	KEY codigo_produto (codigo_produto),
 	CONSTRAINT FK_numero_nota_compra FOREIGN KEY (numero_nota)
-		REFERENCES nota_compra(numero),
+		REFERENCES nota_compra (numero),
 	CONSTRAINT FK_codigo_produto_compra FOREIGN KEY (codigo_produto)
-		REFERENCES produto(codigo)
+		REFERENCES produto (codigo)
 );
 
 CREATE TABLE nota_venda (
@@ -57,9 +56,9 @@ CREATE TABLE item_nota_venda (
 	quantidade INT NOT NULL,
 	preco DECIMAL(7, 2),
 	PRIMARY KEY (numero_nota , codigo_produto),
-	KEY codigo_Produto(codigo_produto),
+	KEY codigo_Produto (codigo_produto),
 	CONSTRAINT FK_numero_nota_venda FOREIGN KEY (numero_nota)
-		REFERENCES nota_venda(numero),
+		REFERENCES nota_venda (numero),
 	CONSTRAINT FK_codigo_produto_venda FOREIGN KEY (codigo_produto)
-		REFERENCES produto(codigo)
+		REFERENCES produto (codigo)
 );
